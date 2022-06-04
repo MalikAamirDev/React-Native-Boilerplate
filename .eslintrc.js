3b5ae63fb8610239f759f9334e95b0b8813eafc3
MalikAamirDev <malikaamirofficial1@gmail.com>
2022-06-04 10:04:56 +0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  singleQuote: true,
+  parser: 'flow',
+};
---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
